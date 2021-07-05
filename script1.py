5b4474c776b76f24224cf1d9a669d4baa36241dd
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2021-07-05 15:37:01 +0530

    added a function

--- script1.py
+++ script1.py
@@ -1,8 +1,12 @@
 from tkinter import *
+def my_func():
+    print(e1_value.get())
+    t1.insert(END,e1_value.get())
 window=Tk()
-b1=Button(window,text="Execute")
+b1=Button(window,text="Execute",command=my_func)
 b1.grid(row=0,column=0)
-e1=Entry(window)
+e1_value=StringVar()
+e1=Entry(window,textvariable=e1_value)
 e1.grid(row=0,column=1)
 t1=Text(window,height=1,width=20)
 t1.grid(row=0,column=2)
